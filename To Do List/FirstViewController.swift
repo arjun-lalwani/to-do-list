31155d8d4d1bb80b70a15a911b24c7fa428f8ca6
Arjun Lalwani <arjunlalwani@D-10-157-119-154.dhcp4.washington.edu>
2016-10-12 11:02:42 -0700

    Add all files

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,17 +8,94 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var items: [String] = []
+    
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    // this function tells the view what to do when the first view is reloaded again
+    // after visiting the second view
+    override func viewDidAppear(_ animated: Bool) {
+        // getting data stored in user defaults
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        
+        if let tempItems = itemsObject as? [String] {
+            
+            items = tempItems
+            
+        }
+        
+        table.reloadData() // reloads data
+    }
+    
+    func tableView (_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle,
+                    forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            
+            items.remove(at: indexPath.row)
+            
+            table.reloadData()
+            
+            // also save this data to user Defaults
+            // after removing item from array
+            UserDefaults.standard.set(items, forKey: "items")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // number of rows
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+       return items.count
+        
+    }
+    
+
+    // text displayed in each row
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        /* alternate way to do optional downcast, rather than force downcast
+         
+         if let tempLabel = items[indexPath.row] as? String { 
+         
+            cellLabel = tempLable
+         
+         }
+ 
+        cell.textLabel?.text = cellLabel 
+         
+        */
+        
+        // as! is a force downcast, where you are sure that the user has entered a String
+        // cell.textLabel?.text = (items[indexPath.row] as! String)
+        
+        // what does textLabel? do?
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+        
+        
+    }
+    
+    
+
 
 
 }
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,8 +8,50 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var itemTextField: UITextField!
+    
+    @IBAction func addButton(_ sender: AnyObject) {
+        
+        // when user adds item, save to permanent storage
+        
+        let itemsObjects = UserDefaults.standard.object(forKey: "items")
+        
+        var items:[String]
+        
+        // NS Array is an immutable array
+        // checking if itemsObjec is a Mutable Array
+        // if it is, storing the Mutable Array in tempItems
+        // tempItems cast itemsObject as an array of Strings
+        if let tempItems = itemsObjects as? [String] {
+            
+            // items points to tempItems
+            items = tempItems
+            
+           // adding text from text field to mutable array
+           // tempItems.addObjects(from: [itemTextField.text])
+            items.append(itemTextField.text!)
+            
+        } else {
+            
+            // if no items are found in UserDefaults, creating single string 
+            // in a new array. this usually happens when 0 items are listed
+            // on the to do list. Thus, it creates a 'new' list with the first 
+            // item being entered by the user.
+            items = [itemTextField.text!]
+            
+        }
+        
+        // stores user defaults
+        UserDefaults.standard.set(items, forKey: "items")
+        
+        // resetting text field, allowing user to quickly add multiple items
+        itemTextField.text = ""
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +62,15 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
